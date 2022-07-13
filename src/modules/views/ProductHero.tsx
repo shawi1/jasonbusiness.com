5798aa24fb84cb1ca0b9b911eb99416a800e2ffd
Brandon Hawi <brandonhawi@jasonbusiness.com>
2022-07-13 12:45:45 -0700

    Start with template

---
+++ src/modules/views/ProductHero.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import Button from '../components/Button';
+import Typography from '../components/Typography';
+import ProductHeroLayout from './ProductHeroLayout';
+
+const backgroundImage =
+  'https://images.unsplash.com/photo-1524431144429-03fdd30eee26?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2888&q=80';
+
+export default function ProductHero() {
+  return (
+    <ProductHeroLayout
+      sxBackground={{
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundColor: '#7fc7d9', // Average color of the background image.
+        backgroundPosition: 'center',
+      }}
+    >
+      {/* Increase the network loading priority of the background image. */}
+      <img
+        style={{ display: 'none' }}
+        src={backgroundImage}
+        alt="increase priority"
+      />
+      <Typography color="inherit" align="center" variant="h2">
+        Where Destiny Meets Realty
+      </Typography>
+      <Button
+        color="secondary"
+        variant="contained"
+        size="large"
+        component="a"
+        href="tel:3215866473"
+        sx={{ minWidth: 200 }}
+      >
+        Call 321-586-6473
+      </Button>
+    </ProductHeroLayout>
+  );
+}
